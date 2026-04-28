44848a44adc29acd66f0fc3ad9dd6d8c16259539
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-04-28 13:01:48 +0200

    first commit

---
+++ src/main/java/com/example/labbaskai/LabbAskAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.labbaskai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabbAskAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabbAskAiApplication.class, args);
+	}
+
+}
